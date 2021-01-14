c6870399ebef4570eb0d250f7838b28ab6bc58af
Jambi 46.2 <69945554+rayna-v@users.noreply.github.com>
2021-01-14 12:00:53 -0700

    fixed typo for name values in post

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -4,14 +4,14 @@ module.exports = function (app) {
     // GET route for getting all of the employees
     app.get('/api/schedule', (req, res) => {
         console.log(req.body)
-        // Write code here to retrieve all of the todos from the database and res.json them back to the user
+
         db.Employee.findAll().then((result) => res.json(result));
     });
 
     app.post('/api/schedule', (req, res) => {
         db.Employee.create({
-            first_name: req.body.first,
-            last_name: req.body.last,
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
             monday: req.body.monday,
             tuesday: req.body.tuesday,
             wednesday: req.body.wednesday,

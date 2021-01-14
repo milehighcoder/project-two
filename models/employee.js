8e90671b45ac0bfcab9b980c508b216ac0785b84
Mike Bannoura <mikebannoura@gmail.com>
2021-01-14 16:14:50 -0700

    Created delete route and put also added email and phone number

--- models/employee.js
+++ models/employee.js
@@ -1,14 +1,16 @@
 module.exports = (sequelize, DataTypes) => {
-    const Employee = sequelize.define('Employee', {
-        first_name: DataTypes.STRING,
-        last_name: DataTypes.STRING,
-        monday: DataTypes.BOOLEAN,
-        tuesday: DataTypes.BOOLEAN,
-        wednesday: DataTypes.BOOLEAN,
-        thursday: DataTypes.BOOLEAN,
-        friday: DataTypes.BOOLEAN,
-        saturday: DataTypes.BOOLEAN,
-        sunday: DataTypes.BOOLEAN,
-    });
-    return Employee;
+  const Employee = sequelize.define("Employee", {
+    first_name: DataTypes.STRING,
+    last_name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.INTEGER,
+    monday: DataTypes.BOOLEAN,
+    tuesday: DataTypes.BOOLEAN,
+    wednesday: DataTypes.BOOLEAN,
+    thursday: DataTypes.BOOLEAN,
+    friday: DataTypes.BOOLEAN,
+    saturday: DataTypes.BOOLEAN,
+    sunday: DataTypes.BOOLEAN,
+  });
+  return Employee;
 };

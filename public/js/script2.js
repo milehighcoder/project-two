6f08af99fa1917922949e2997dc08482c4c28d29
Riley Costello <rileytcostello@gmail.com>
2021-01-14 21:01:19 -0700

    adding mychaels script and fixing last push

--- public/js/script2.js
+++ public/js/script2.js
@@ -1,63 +1,72 @@
-var display = document.getElementById('timer');
+var display = document.getElementById("timer");
 var secs = 0;
 var mins = 0;
-var hrs  = 0;
+var hrs = 0;
 var h = "";
 var m = "";
 var s = "";
 var timer;
- 
-function startTimer(btn){
-	btn.setAttribute('disabled', 'disabled');
-	durationTime();
- 
+// Get the modal
+var modal = document.getElementById("profile-modal");
+// Get the button that opens the modal
+var btn = document.getElementById("update-profile-modal");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+function startTimer(btn) {
+  btn.setAttribute("disabled", "disabled");
+  durationTime();
 }
- 
- 
-function stopTimer(){
-	document.getElementsByClassName('button button-rounded-hover')[0].removeAttribute('disabled');
-	clearTimeout(timer);
+function stopTimer() {
+  document
+    .getElementsByClassName("button button-rounded-hover")[0]
+    .removeAttribute("disabled");
+  clearTimeout(timer);
 }
- 
-function resetTimer(){
-	document.getElementsByClassName('button button-rounded-hover')[0].removeAttribute('disabled');
-	clearTimeout(timer);
-	display.innerHTML = "00:00:00";
-	secs = 0; 
-	mins = 0; 
-	hrs = 0;
-	h = "";
-	m = "";
-	s = "";
-
-	console.log(timer)
-
+function resetTimer() {
+  document
+    .getElementsByClassName("button button-rounded-hover")[0]
+    .removeAttribute("disabled");
+  clearTimeout(timer);
+  display.innerHTML = "00:00:00";
+  secs = 0;
+  mins = 0;
+  hrs = 0;
+  h = "";
+  m = "";
+  s = "";
 }
- 
-function countTimer(){
-	secs++;
- 
-	if(secs >= 60){
-		secs = 0;
-		mins++;
-		if(mins >= 60){
-			mins = 0;
-			hrs++;
-		}
-	}
- 
-	h = hrs ? hrs > 9 ? hrs : "0" + hrs : "00";
-	m = mins ? mins > 9 ? mins : "0" + mins : "00";
-	s = secs > 9 ? secs : "0" + secs;
- 
-	display.innerHTML = h+":"+m+":"+s;
- 
-	durationTime();
+function countTimer() {
+  secs++;
+  if (secs >= 60) {
+    secs = 0;
+    mins++;
+    if (mins >= 60) {
+      mins = 0;
+      hrs++;
+    }
+  }
+  h = hrs ? (hrs > 9 ? hrs : "0" + hrs) : "00";
+  m = mins ? (mins > 9 ? mins : "0" + mins) : "00";
+  s = secs > 9 ? secs : "0" + secs;
+  display.innerHTML = h + ":" + m + ":" + s;
+  durationTime();
 }
- 
-function durationTime(){
-	if(hrs != 99){
-		timer = setTimeout(countTimer, 100);
-	}
- 
+function durationTime() {
+  if (hrs != 99) {
+    timer = setTimeout(countTimer, 100);
+  }
 }

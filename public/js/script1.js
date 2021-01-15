9f4528917357db005dafdc325f901677e7c9a90c
mykereel <mgmartnz@icloud.com>
2021-01-14 19:54:05 -0700

    my profile modal

--- public/js/script1.js
+++ public/js/script1.js
@@ -1,5 +1,6 @@
-$('[data-mobile-app-toggle] .button').click(function () {
-    $(this).siblings().removeClass('is-active');
-    $(this).addClass('is-active');
-  });
-  
+$("[data-mobile-app-toggle] .button").click(function () {
+  $(this).siblings().removeClass("is-active");
+  $(this).addClass("is-active");
+});
+
+

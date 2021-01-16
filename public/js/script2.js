e66e036071dbe41bc41cff4d089bfe622727426b
mykereel <mgmartnz@icloud.com>
2021-01-16 11:58:42 -0700

    update work

--- public/js/script2.js
+++ public/js/script2.js
@@ -6,26 +6,45 @@ var h = "";
 var m = "";
 var s = "";
 var timer;
+
+// START MODAL SCRIPT |||||||||||||||||||||||||||
+
 // Get the modal
-var modal = document.getElementById("profile-modal");
+var modal = document.getElementsByClassName("modal");
+
 // Get the button that opens the modal
-var btn = document.getElementById("update-profile-modal");
+var btn = document.getElementsByClassName("myBtn");
+
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks on the button, open the modal
-btn.onclick = function () {
-  modal.style.display = "block";
+var span = document.getElementsByClassName("close");
+
+// When the user clicks the button, open the modal
+btn[0].onclick = function () {
+  modal[0].style.display = "block";
 };
+
+btn[1].onclick = function () {
+  modal[1].style.display = "block";
+};
+
 // When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-  modal.style.display = "none";
+span[0].onclick = function () {
+  modal[0].style.display = "none";
 };
+
+span[1].onclick = function () {
+  modal[1].style.display = "none";
+};
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 };
+
+// END MODAL SCRIPT |||||||||||||||||||||||||||||||
+
 function startTimer(btn) {
   btn.setAttribute("disabled", "disabled");
   durationTime();
@@ -48,7 +67,7 @@ function resetTimer() {
   h = "";
   m = "";
   s = "";
-  console.log(timer)
+  console.log(timer);
 }
 function countTimer() {
   secs++;

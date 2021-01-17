51d64ed629ff39f3476eec0d988b9d5d2856de16
Mike Bannoura <mikebannoura@gmail.com>
2021-01-17 02:09:28 -0700

    implement update profile

--- public/js/user.js
+++ public/js/user.js
@@ -1,35 +1,62 @@
-// document.addEventListener("DOMContentLoaded", (e) => {
-//   const loginButton = document.getElementById("sign-in-form-login-btn");
+const firstNameField = document.getElementById("update-first-name");
+const lastNameField = document.getElementById("update-last-name");
+const phoneNumberField = document.getElementById("update-phone");
+const emailField = document.getElementById("update-email");
+let userId;
 
-//   const getUserSchedule = () => {
-//     console.log("logging in");
-//     const email = document.getElementById("sign-in-form-username").value;
-//     const password = document.getElementById("sign-in-form-password").value;
-//     fetch("/auth/login", {
-//       method: "POST",
-//       body: JSON.stringify({
-//         email,
-//         password,
-//       }),
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//     })
-//       .then((response) => response.json())
-//       .then((data) => {
-//         const {
-//           token,
-//           // user: { isManager },
-//           user: email,
-//         } = data;
-//         localStorage.setItem("token", token);
-//         // location.href = isManager ? "/manager" : "/user";
-//         location.href = "/portal"
-//       });
-//   };
+const showUpdateProfileButton = document.getElementById(
+  "show-update-profile-button"
+);
+const updateProfileButton = document.getElementById("update-profile-button");
 
-//   loginButton.addEventListener("click", (event) => {
-//     event.preventDefault();
-//     login();
-//   });
-// });
+const getUserDetails = () => {
+  fetch("/me", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      "x-token": localStorage.getItem("token"),
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("data: ", data);
+      const { first_name, last_name, email, phone, userId: id } = data;
+      firstNameField.value = first_name;
+      lastNameField.value = last_name;
+      phoneNumberField.value = phone;
+      emailField.value = email;
+      userId = id;
+      document.getElementById("manager-name-container").innerText =
+        first_name + " " + last_name;
+    });
+};
+
+const updateUserDetails = () => {
+  const first_name = firstNameField.value;
+  const last_name = lastNameField.value;
+  const phone = phoneNumberField.value;
+  const email = emailField.value;
+
+  fetch("/updateDetails", {
+    method: "POST",
+    body: JSON.stringify({
+      first_name,
+      last_name,
+      email,
+      phone,
+      userId,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+      "x-token": localStorage.getItem("token"),
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+    });
+};
+
+// showUpdateProfileButton.addEventListener("click", );
+getUserDetails();
+updateProfileButton.addEventListener("click", updateUserDetails);
